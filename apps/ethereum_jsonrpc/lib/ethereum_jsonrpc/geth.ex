e17529149123d4e609b747bcbb9e4603ef226276
Mario Sánchez García <mario@purestake.com>
2021-09-17 14:43:48 +0200

    test: rm unused Transactions alias

--- apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/geth.ex
+++ apps/ethereum_jsonrpc/lib/ethereum_jsonrpc/geth.ex
@@ -5,7 +5,7 @@ defmodule EthereumJSONRPC.Geth do
 
   import EthereumJSONRPC, only: [id_to_params: 1, integer_to_quantity: 1, json_rpc: 2, request: 1]
 
-  alias EthereumJSONRPC.{FetchedBalance, FetchedCode, Transactions}
+  alias EthereumJSONRPC.{FetchedBalance, FetchedCode}
   alias EthereumJSONRPC.Geth.{Calls, Tracer}
 
   @behaviour EthereumJSONRPC.Variant
